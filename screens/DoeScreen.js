7597e4740dd2855a1efec3cf5a742920d38f9f4f
giovannvfs <giovannivinicius1997@gmail.com>
2020-03-04 12:25:36 -0300

    Implementação validações

--- screens/DoeScreen.js
+++ screens/DoeScreen.js
@@ -1,34 +1,55 @@
 import React, { Component } from "react";
-import {
-  ScrollView,
-  StyleSheet,
-  View,
-  StatusBar,
-  Text,
-  Alert
-} from "react-native";
+import { StyleSheet, View, Alert } from "react-native";
 import { SafeAreaView } from "react-navigation";
-import { Input } from "react-native-elements";
+import { Input, Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import SwitchSelector from "react-native-switch-selector";
-import {
-  CreditCardInput,
-  LiteCreditCardInput
-} from "react-native-input-credit-card";
+import { CreditCardInput } from "react-native-input-credit-card";
+import { TextInputMask } from "react-native-masked-text";
+import AwesomeAlert from "react-native-awesome-alerts";
 
+console.disableYellowBox = true;
 
 export default class DoeScreen extends Component {
-
   state = {
     dados: {},
-    valid: false,
-    type: "credit"
+    valido: false,
+    tipo: "credit",
+    valor: "0.00",
+    alertaConfirmacao: false,
+    alertaTitulo: "",
+    alertaTexto: "",
+    alertaBtnOk: true,
+    alertaBtnOkTexto: "Ok",
+    alertaBtnNok: false,
+    doar: false
+
+  };
+
+  alteraCartao = formData => {
+    this.setState({ dados: formData.values, valido: formData.valid });
+    this.state.valido
+  };
+
+  alteraValor(valor) {
+    this.setState({ valor });
   }
 
+  validar() {
+    if (
+      parseInt(this.state.valor.replace("R$", "").replace(".", "").replace(",", "")) === 0) {
+      this.setState({alertaConfirmacao: true,alertaTitulo: "Atenção", alertaTexto: "O valor para doação deve ser maior que R$ 0,00", doar: false })
+    } else if (this.state.valido === false) {
+      this.setState({alertaConfirmacao: true,alertaTitulo: "Atenção", alertaTexto: "Dados do cartão inválidos favor tente novamente", doar: false })
+    } else {
+      this.setState({alertaConfirmacao: true, alertaTitulo: "Doação", alertaTexto: "Confirmar doação?", alertaBtnNok: true, alertaBtnOkTexto: "Confirmar", doar: true})
+    }
+  }
 
-
-  _onChange = (formData) => {
-    this.setState({dados: formData.values, valid: formData.valid})
-  } 
+  doar() {
+    this.setState({alertaConfirmacao: false, doar: false, dados: []})
+    this.forceUpdate()
+  }
 
   render() {
     const options = [
@@ -39,15 +60,32 @@ export default class DoeScreen extends Component {
     return (
       <SafeAreaView style={styles.container}>
         <SwitchSelector
+          style={{ marginBottom: 25 }}
           options={options}
           buttonColor="#B10403"
           borderColor="#B10403"
           buttonMargin={3}
           hasPadding
           initial={0}
-          onPress={value => this.setState({type: value})}
+          onPress={value => this.setState({ type: value })}
         />
-        <View style={{ marginTop: 20 }}>
+        <View style={styles.containerRow}>
+          <TextInputMask
+            type={"money"}
+            options={{
+              precision: 2,
+              separator: ",",
+              delimiter: ".",
+              unit: "R$",
+              suffixUnit: ""
+            }}
+            value={this.state.valor}
+            onChangeText={text => this.alteraValor(text)}
+            style={styles.maskInput}
+          />
+        </View>
+
+        <View style={{ marginTop: 25 }}>
           <CreditCardInput
             labels={{ number: "Número Cartão", expiry: "Validade", cvc: "CVC" }}
             placeholders={{
@@ -56,9 +94,36 @@ export default class DoeScreen extends Component {
               cvc: "CVC"
             }}
             allowScroll
-            onChange={this._onChange}
+            onChange={this.alteraCartao}
           />
         </View>
+        <Button
+          buttonStyle={{ backgroundColor: "#B10403" }}
+          icon={<Icon name="hand-holding-heart" size={15} color="white" />}
+          title=" Doar"
+          onPress={() => this.validar()}
+        />
+
+        <AwesomeAlert
+          show={this.state.alertaConfirmacao}
+          showProgress={false}
+          title={this.state.alertaTitulo}
+          message={this.state.alertaTexto}
+          closeOnTouchOutside={false}
+          closeOnHardwareBackPress={false}
+          showCancelButton={this.state.alertaBtnNok}
+          showConfirmButton={this.state.alertaBtnOk}
+          cancelText="Cancelar"
+          cancelButtonColor="#B10403"
+          confirmText={this.state.alertaBtnOkTexto}
+          confirmButtonColor="#0e8a00"
+          onCancelPressed={() => {
+            this.setState({alertaConfirmacao: false})
+          }}
+          onConfirmPressed={() => {
+            this.state.doar ? this.doar() :  this.setState({alertaConfirmacao: false})
+          }}
+        />
       </SafeAreaView>
     );
   }
@@ -74,5 +139,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     padding: 10,
     marginTop: 10
+  },
+  containerRow:{
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  maskInput: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#000",
+    fontSize: 50,
+    textAlign: "center",
+    width: 280
   }
 });
